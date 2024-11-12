8a288410c2e60023297fa62eed8c2836fe2053a6
Jagatjit Turuk <jagatjit@gmail.com>
2024-11-12 13:56:40 +0530

    Chat working

--- src/api/chat_basic_ai_inference.py
+++ src/api/chat_basic_ai_inference.py
@@ -22,8 +22,9 @@ USAGE:
 """
 
 import os
+from typing import Any, Mapping, Optional, overload
 from azure.ai.projects import AIProjectClient
-from azure.ai.inference.models import UserMessage
+from azure.ai.inference.models import UserMessage, ChatRequestMessage, ChatResponseMessage
 from azure.identity import DefaultAzureCredential
 import fastapi
 from urllib.parse import urlparse
@@ -36,6 +37,9 @@ print(os.environ)
 
 #project_connection_string ="northcentralus.api.azureml.ms;f93b65a5-0bac-4e59-b286-140e2dae416d;rg-jturuk-8571_ai;jturuk-5739"
 #project_connection_string = "northcentralus.api.azureml.ms;f93b65a5-0bac-4e59-b286-140e2dae416d;rg-azureai-basic-python;ai-project-iyjj47eku46ow"
+#project_connection_string = "eastus2.api.azureml.ms;597966d1-829f-417e-9950-8189061ec09c;rg-dantaylo-e2e-demo2;dantaylo-e2e-demo2"
+#model_deployment_name ="gpt-4o-mini"
+
 project_connection_string = urlparse(os.environ["AZUREAI_PROJECT_DISCOVERYURL"]).netloc +";"+os.environ["AZURE_SUBSCRIPTION_ID"]+";"+os.environ["AZURE_RESOURCE_GROUP"]+";"+os.environ["AZUREAI_PROJECT_NAME"]
 model_deployment_name ="gpt-35-turbo-16k"
 
@@ -58,11 +62,21 @@ def generate_chat(input):
     )
     return response.choices[0].message.content
 
-def generate_chat_stream(input):
-    print("input 2 "+input)
+def generate_chat_stream(messages):
+    body = {
+        "model": model_deployment_name,
+        "messages": messages,
+        "stream": True
+    }
+
     project_client = AIProjectClient.from_connection_string(credential=DefaultAzureCredential(),conn_str=project_connection_string)
+
 
     client = project_client.inference.get_chat_completions_client()
     
-    response = client.complete(model=model_deployment_name, messages=[UserMessage(content=input)],stream=True)
-    return fastapi.responses.StreamingResponse(response, media_type='text/event-stream')
+    completion = client.complete(model = model_deployment_name, messages=messages, stream=True)
+    
+    print(f"Completion: {completion}")
+    
+    return completion
+ 
